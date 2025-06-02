2e66dec663bf78f017e7878508669b038390cf11
sabahabtamu <sabahabtamuu@gmail.com>
2025-06-03 00:04:12 +0300

    Add expense detail screen with provider

---
+++ wander_wallet/lib/features/expenses/presentation/providers/expense_details_provider.dart
@@ -0,0 +1,79 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wander_wallet/core/di/providers.dart';
+import 'package:wander_wallet/core/models/error.dart';
+import 'package:wander_wallet/core/models/payload.dart';
+import 'package:wander_wallet/core/models/result.dart';
+import 'package:wander_wallet/features/expenses/domain/expenses_repository.dart';
+
+sealed class ExpenseDetailsScreenState {
+  ExpenseDetailsScreenState();
+}
+
+class ExpenseDetailsGetSuccess extends ExpenseDetailsScreenState {
+  final ExpensePayload expensePayload;
+
+  ExpenseDetailsGetSuccess(this.expensePayload);
+}
+
+class ExpenseDetailsDeleteSuccess extends ExpenseDetailsScreenState {
+  final MessagePayload messagePayload;
+
+  ExpenseDetailsDeleteSuccess(this.messagePayload);
+}
+
+class ExpenseDetailsError extends ExpenseDetailsScreenState {
+  final MessageError error;
+  final bool loggedOut;
+
+  ExpenseDetailsError(this.error, this.loggedOut);
+}
+
+class ExpenseDetailsScreenNotifier extends FamilyAsyncNotifier<ExpenseDetailsScreenState, String> {
+  ExpenseDetailsScreenNotifier();
+  @override
+  Future<ExpenseDetailsScreenState> build(String id) async {
+    final expensesRepository = ref.read(expensesRepositoryProvider);
+    state = AsyncValue.loading();
+    final res = await expensesRepository.getExpense(arg);
+    if (res is Success) {
+      return ExpenseDetailsGetSuccess((res as Success).data);
+    } else {
+      throw ExpenseDetailsError(
+        (res as Error).error,
+        (res as Error).loggedOut
+      );
+    }
+  }
+
+  Future<void> deleteExpense() async {
+    final expensesRepository = ref.read(expensesRepositoryProvider);
+    state = AsyncValue.loading();
+    final res = await expensesRepository.deleteExpense(arg);
+    if (res is Success) {
+      state = AsyncValue.data(ExpenseDetailsDeleteSuccess((res as Success).data));
+    } else {
+      state = AsyncValue.error(
+        ExpenseDetailsError((res as Error).error, (res as Error).loggedOut),
+        StackTrace.current
+      );
+    }
+  }
+
+  Future<void> refresh() async {
+    final expensesRepository = ref.read(expensesRepositoryProvider);
+    state = AsyncValue.loading();
+    final res = await expensesRepository.getExpense(arg);
+    if (res is Success) {
+      state = AsyncValue.data(ExpenseDetailsGetSuccess((res as Success).data));
+    } else {
+      state = AsyncValue.error(
+        ExpenseDetailsError((res as Error).error, (res as Error).loggedOut),
+        StackTrace.current
+      );
+    }
+  }
+}
+
+final expenseDetailsProvider = AsyncNotifierProvider.family<ExpenseDetailsScreenNotifier, ExpenseDetailsScreenState, String>(
+  ExpenseDetailsScreenNotifier.new
+);
---
+++ wander_wallet/lib/features/expenses/presentation/screens/expense_details_screen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wander_wallet/core/widgets/buttons.dart';
+import 'package:wander_wallet/core/widgets/expense_info.dart';
+import 'package:wander_wallet/features/expenses/presentation/providers/edit_expense_provider.dart';
+import 'package:wander_wallet/features/expenses/presentation/providers/expense_details_provider.dart';
+import 'package:wander_wallet/features/trips/presentation/providers/trip_details_provider.dart';
+
+class ExpenseDetailsScreen extends ConsumerStatefulWidget {
+  final String id;
+  final String tripId;
+
+  const ExpenseDetailsScreen({ super.key, required this.id, required this.tripId });
+
+  @override
+  ConsumerState<ExpenseDetailsScreen> createState() => _ExpenseDetailsScreenState();
+}
+
+class _ExpenseDetailsScreenState extends ConsumerState<ExpenseDetailsScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.listenManual(expenseDetailsProvider(widget.id), (prev, next) {
+      next.when(
+        data: (data) {
+          if (data is ExpenseDetailsDeleteSuccess) {
+            ref.invalidate(tripDetailsProvider(widget.tripId));
+            Navigator.pushNamed(context, '/tripDetails', arguments: widget.tripId);
+          }
+        },
+        loading: () {},
+        error: (error, _) {
+          if (error is ExpenseDetailsError && error.loggedOut) {
+            Navigator.pushNamed(context, '/login');
+          }
+        }
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final expenseDetailsState = ref.watch(expenseDetailsProvider(widget.id));
+    final theme = Theme.of(context);
+
+    return expenseDetailsState.when(
+      data: (data) {
+        if (data is ExpenseDetailsDeleteSuccess) {
+          return Center(
+            child: Text('Expense deleted successfully!'),
+          );
+        } else {
+          final expense = (data as ExpenseDetailsGetSuccess).expensePayload.expense;
+
+          return Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              spacing: 28,
+              children: [
+                ExpenseInfo(expense: expense),
+                Row(
+                  spacing: 16,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RectangularButton(
+                      onPressed: () {
+                        ref.invalidate(editExpenseProvider(expense.id));
+                        Navigator.pushNamed(context, '/editExpense', arguments: expense.id);
+                      },
+                      text: 'Edit',
+                    ),
+                    RectangularButton(
+                      color: theme.colorScheme.error,
+                      textColor: theme.colorScheme.onError,
+                      onPressed: () {
+                        ref.read(expenseDetailsProvider(widget.id).notifier).deleteExpense();
+                      },
+                      text: 'Delete',
+                    )
+                  ],
+                ),
+              ],
+            ),
+          );
+        }
+      },
+      error: (err, _) => Center(
+        child: Column(
+          spacing: 16,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text((err as ExpenseDetailsError).error.message),
+            RectangularButton(
+              onPressed: () {
+                ref.read(expenseDetailsProvider(widget.id).notifier).refresh();
+              },
+              text: 'Retry',
+            )
+          ],
+        ),
+      ),
+      loading: () => Center(child: CircularProgressIndicator())
+    );
+  }
+}
