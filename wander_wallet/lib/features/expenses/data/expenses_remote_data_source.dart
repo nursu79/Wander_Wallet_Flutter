c02abcae167e39a50613c4a3fe4b3b5096b51c44
sabahabtamu <sabahabtamuu@gmail.com>
2025-06-02 21:34:39 +0300

    Implement update and delete expense API calls

--- wander_wallet/lib/features/expenses/data/expenses_remote_data_source.dart
+++ wander_wallet/lib/features/expenses/data/expenses_remote_data_source.dart
@@ -38,4 +38,36 @@ class ExpensesRemoteDataSource {
     return res;
   }
 
+  Future<Response<dynamic>> updateExpense(
+    String id,
+    String name,
+    num amount,
+    Category category,
+    DateTime date,
+    String? notes
+  ) async {
+    final res = await dio.put(
+      ApiConstants.getExpensePath(id),
+      data: {
+        'name': name,
+        'amount': amount,
+        'category': category.name,
+        'date': date.toIso8601String(),
+        'notes': notes
+      }
+    );
+
+    return res;
+  }
+
+  Future<Response<dynamic>> deleteExpense(
+    String id
+  ) async {
+    final res = await dio.delete(
+      ApiConstants.getExpensePath(id)
+    );
+
+    return res;
+  }
+
 }
