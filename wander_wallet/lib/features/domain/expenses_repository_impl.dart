2b245b3c930b6bc968e58dc864efc51e91973520
sabahabtamu <sabahabtamuu@gmail.com>
2025-06-02 19:38:43 +0300

    implement create expense operation

--- wander_wallet/lib/features/domain/expenses_repository_impl.dart
+++ wander_wallet/lib/features/domain/expenses_repository_impl.dart
@@ -10,4 +10,27 @@ class ExpensesRepositoryImpl implements ExpensesRepository {
   final ExpensesRemoteDataSource remote;
 
   ExpensesRepositoryImpl(this.remote);
+
+  @override
+  Future<Result<ExpensePayload, ExpenseError>> createExpense(String tripId, String name, num amount, Category category, DateTime date, String? notes) async {
+    try {
+      final res = await remote.createExpense(tripId, name, amount, category, date, notes);
+      final expensePayload = ExpensePayload.fromJson(res.data);
+      return Success(expensePayload);
+    } on DioException catch (e) {
+      if (e.response != null) {
+        final expenseError = ExpenseError.fromJson(e.response?.data);
+        return Error(error: expenseError, loggedOut: e.response?.statusCode == 401);
+      } else {
+        return Error(
+          error: ExpenseError(message: 'Please check your internet connection and/or api address'),
+          loggedOut: false,
+        );
+      }
+    } on Exception {
+      final expenseError = ExpenseError(message: 'An unexpected error occurred');
+      return Error(error: expenseError);
+    }
+  }
+  
 }
